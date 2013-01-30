630ca580ed94cb9450e34aad5e12d93b5198945b
numeroteca <pablo@basurama.org>
2013-01-30 01:23:57 -0500

    add Case Study custom post type to functions.php

--- functions.php
+++ functions.php
@@ -10,4 +10,40 @@ function register_my_menu() {
                 );
         }
 }
+
+// Custom post types
+add_action( 'init', 'create_post_type', 0 );
+
+function create_post_type() {
+	// Case Studies custom post type
+	register_post_type( 'case', array(
+		'labels' => array(
+			'name' => __( 'Case Study' ),
+			'singular_name' => __( 'Case Study' ),
+			'add_new_item' => __( 'Add Case Study' ),
+			'edit' => __( 'Edit' ),
+			'edit_item' => __( 'Edit this Case Study' ),
+			'new_item' => __( 'New Case Studyt' ),
+			'view' => __( 'View Case Study' ),
+			'view_item' => __( 'View this Case Study' ),
+			'search_items' => __( 'Search Case Studies' ),
+			'not_found' => __( 'No Case Study was found' ),
+			'not_found_in_trash' => __( 'No Case Study in the trash' ),
+			'parent' => __( 'Parent' )
+		),
+		'public' => true,
+		'publicly_queryable' => true,
+		'exclude_from_search' => false,
+		'menu_position' => 5,
+		//'menu_icon' => get_template_directory_uri() . '/images/icon-post.type-integrantes.png',
+		'hierarchical' => true, // if true this post type will be as pages
+		'query_var' => true,
+		'supports' => array('title', 'editor','excerpt','custom-fields','author','page-attributes','trackbacks','thumbnail','comments' ),
+		'rewrite' => array('slug'=>'case','with_front'=>false),
+		'can_export' => true,
+		'_builtin' => false,
+		'_edit_link' => 'post.php?post=%d',
+	));
+
+
 ?>
