3fd90e4cc0a6faa5b71a39b83826d35a9cf86db3
skotperez <voraginebramante@gmail.com>
2013-02-06 17:20:35 +0100

    single case studies sidebar as a table, instead as a list

--- loop.single.php
+++ loop.single.php
@@ -17,8 +17,11 @@
 <div id="case-img" class="row">
 	<div class="container">
 		<div class="row">
-			<div class="span12">
-	<?php the_post_thumbnail('medium'); the_post_thumbnail('medium');?>	
+			<div class="span6">
+	<?php the_post_thumbnail('medium'); ?>	
+			</div>
+			<div class="span6">
+	<?php the_post_thumbnail('medium'); ?>	
 			</div>
 		</div>
 	</div>
@@ -42,26 +45,78 @@
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="span3">
-			<div class="well sidebar-nav">
-				<ul class="nav nav-list">
-					<li class="nav-header">Other Density Measures</li>
-					<li><?php echo 'DUs/Ha ' .get_post_meta( $post->ID, 'dus-ha', true ); ?></li>
-					<li><?php echo 'm2/Ha ' .get_post_meta( $post->ID, 'm2-ha', true ); ?></li>
-					<li class="nav-header">Project data</li>
-					<li><?php echo 'Gross Building Area ' .get_post_meta( $post->ID, 'gross-building-area', true ); ?></li>
-					<li><?php echo 'Site Area ' .get_post_meta( $post->ID, 'site-area', true ); ?></li>
-					<li><?php echo 'Range of Heights ' .get_post_meta( $post->ID, 'range-of-heights', true ); ?></li>
-					<li><?php echo 'Dwelling Units ' .get_post_meta( $post->ID, 'dwelling-units', true ); ?></li>
-					<li><?php echo 'Parking Spaces ' .get_post_meta( $post->ID, 'parking-spaces', true ); ?></li>
-					<li class="nav-header">People</li>
-					<li><?php echo 'Population ' .get_post_meta( $post->ID, 'population', true ); ?></li>
-					<li><?php echo 'Income ' .get_post_meta( $post->ID, 'income', true ); ?></li>
-					<li><?php echo 'Demographic group ' .get_post_meta( $post->ID, 'demographic-group', true ); ?>
-					<li class="nav-header">References</li>
-					<li><?php echo 'References ' .get_post_meta( $post->ID, 'references', true );?></li>
-
-		<?php echo 'Location ' .get_post_meta( $post->ID, 'location', true ); ?>
-			</div>
+			<table class="table table-condensed">
+				<thead>
+					<tr><th>Other Density Measures</th></tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?php echo 'DUs/Ha' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'dus-ha', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'm2/Ha' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'm2-ha', true ); ?></td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table table-condensed">
+				<thead>
+					<tr><th>Project data</th></tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?php echo 'Gross Building Area' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'gross-building-area', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Site Area' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'site-area', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Range of Heights' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'range-of-heights', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Dwelling Units' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'dwelling-units', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Parking Spaces' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'parking-spaces', true ); ?></td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table table-condensed">
+				<thead>
+					<tr><th>People</th></tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?php echo 'Population' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'population', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Income' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'income', true ); ?></td>
+					</tr>
+					<tr>
+						<td><?php echo 'Demographic group' ?></td>
+						<td><?php echo get_post_meta( $post->ID, 'demographic-group', true ); ?></td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table table-condensed">
+				<thead>
+					<tr><th>References</th></tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><?php echo get_post_meta( $post->ID, 'references', true ); ?></td>
+					</tr>
+				</tbody>
+			</table>
+		<?php //echo 'Location ' .get_post_meta( $post->ID, 'location', true ); ?>
 		</div>
 		<div class="span9">
 			<h2><?php the_title();?></h2>
