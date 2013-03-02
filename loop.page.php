c83feb0b163980f0e256678525f43236e4543a36
numeroteca <pablo@basurama.org>
2013-03-02 12:26:43 -0500

    adds basic visualization of pages

--- loop.page.php
+++ loop.page.php
@@ -1,5 +1,4 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>> 
-	
 	<?php if (is_page(array(4,'case-studies'))) { //conditional if is Case Studies page ?>
 	<?php } else { ?>
 	<h2><?php the_title();?></h2>
--- page.php
+++ page.php
@@ -1,15 +1,22 @@
-<?php 
-get_header();
-?>
+<?php get_header(); ?>
 
-<div id="content" class="span9">
-	<?php 
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
-			include("loop.page.php");
-		endwhile;
-	else :
-	endif;?>
-</div><!-- #content -->
+<div id="page">
+	<div class="container">
+		<div class="row">
+			<div id="case-sidebar" class="span3"> -</div>
+			<div class="span6 offset1">
+				<?php 
+				if ( have_posts() ) :
+					while ( have_posts() ) : the_post();
+						include("loop.page.php");
+					endwhile;
+				else :
+				endif;?>
+			</div>
+		</div>
+	</div>
+</div><!-- #page -->
 
 <?php get_footer(); ?>
+
+
